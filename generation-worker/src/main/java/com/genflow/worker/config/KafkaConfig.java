de981c7ad1d2b2785ba05a9086083db7cb5bce16
Yogender <yashmittal92002@gmail.com>
2026-09-23 12:04:31 +0530

    Add Kubernetes deployment for generation worker

--- generation-worker/src/main/java/com/genflow/worker/config/KafkaConfig.java
+++ generation-worker/src/main/java/com/genflow/worker/config/KafkaConfig.java
@@ -3,22 +3,27 @@ package com.genflow.worker.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
@@ -27,7 +32,7 @@ public class KafkaConfig {
 
         config.put(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-            "localhost:9092"
+            bootstrapServers
         );
 
         config.put(
@@ -49,47 +54,47 @@ public class KafkaConfig {
     }
 
     @Bean
-public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory() {
 
-    Map<String, Object> config = new HashMap<>();
+        Map<String, Object> config = new HashMap<>();
 
-    config.put(
-        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-        "localhost:9092"
-    );
+        config.put(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            bootstrapServers
+        );
 
-    config.put(
-        ConsumerConfig.GROUP_ID_CONFIG,
-        "genflow-group"
-    );
+        config.put(
+            ConsumerConfig.GROUP_ID_CONFIG,
+            "genflow-group"
+        );
 
-    config.put(
-        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-        StringDeserializer.class
-    );
+        config.put(
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+            StringDeserializer.class
+        );
 
-    config.put(
-        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-        StringDeserializer.class
-    );
+        config.put(
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+            StringDeserializer.class
+        );
 
-    config.put(
-        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
-        "earliest"
-    );
+        config.put(
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+            "earliest"
+        );
 
-    return new DefaultKafkaConsumerFactory<>(config);
-}
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
 
-@Bean
-public ConcurrentKafkaListenerContainerFactory<String, String>
-        kafkaListenerContainerFactory() {
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+            kafkaListenerContainerFactory() {
 
-    ConcurrentKafkaListenerContainerFactory<String, String> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
 
-    factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerFactory());
 
-    return factory;
-}
+        return factory;
+    }
 }
